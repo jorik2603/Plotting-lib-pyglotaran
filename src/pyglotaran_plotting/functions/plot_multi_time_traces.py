6cd01722c0ea230d42d1c2eb86a359b4874ed2bd
jorik2603 <150458572+jorik2603@users.noreply.github.com>
2026-06-03 13:32:08 +0200

    Updated export logic to include chirp correction

--- src/pyglotaran_plotting/functions/plot_multi_time_traces.py
+++ src/pyglotaran_plotting/functions/plot_multi_time_traces.py
@@ -122,7 +122,9 @@ def plot_multi_time_traces(datasets, dataset_labels, spectral_values,
                 if export:
                     path = Path(export_folder)
                     path.mkdir(parents=True, exist_ok=True)
-                    export_var = data_slice.to_dataframe()
+                    export_data = data_slice.values
+                    export_time = time_coords_for_plot.values
+                    export_var = np.column_stack(export_time,export_data)
                     export_var.to_csv(export_folder+"/"+legend_label+"extracted.csv")
 
             except Exception as e:
